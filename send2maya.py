fa62b8ec3eca4698cdc7b523c51bc7daf23cc6cb
JuanJoII <juanjose34251@gmail.com>
2025-09-21 21:08:29 -0500

    send2maya updated and fixed

--- send2maya.py
+++ send2maya.py
@@ -2,39 +2,24 @@ import socket
 import sys
 from pathlib import Path
 
-# Ip local y puerto por defecto de Maya
 HOST = "127.0.0.1"
 PORT = 4434
 
-
-def enviar_a_maya(script_text: str):
+def enviar_archivo_a_maya(file_path: str):
+    safe_path = str(file_path).replace("\\", "/")
+    maya_cmd  = f"exec(open(r'{safe_path}', encoding='utf-8').read())"
     try:
-        # Crear socket y conectar a Maya
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((HOST, PORT))
-            # Enviar cada línea del script 
-            for line in script_text.splitlines():
-                if line.strip():  # evitar líneas vacías
-                    comando = line.replace('"', '\\"') 
-                    s.send(f'python("{comando}")\r\n'.encode()) # Enviar todo a Maya
-            print("✅ Script enviado a Maya")
+            s.send(f'python("{maya_cmd}")\r\n'.encode())
+            print(f"✅ Ejecutado en Maya con UTF-8: {safe_path}")
     except Exception as e:
-        print("❌ Error al enviar a Maya:", e)
-
-# Función main recibe el archivo .py como argumento, pathlib se encarga de leer el archivo para procesarlo en la función anterior
-def main():
-    if len(sys.argv) < 2:
-        print("⚠️ Debes pasar un archivo .py como argumento")
-        sys.exit(1)
-
-    file_path = Path(sys.argv[1])
-    if not file_path.exists():
-        print("⚠️ Archivo no encontrado:", file_path)
-        sys.exit(1)
-
-    script_text = file_path.read_text(encoding="utf-8")
-    enviar_a_maya(script_text)
-
+        print("❌ Error:", e)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 2:
+        print("⚠️ Pasa el archivo .py como argumento")
+        sys.exit(1)
+
+    file_path = Path(sys.argv[1]).resolve()
+    enviar_archivo_a_maya(file_path)
